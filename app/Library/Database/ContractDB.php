9840823ed10f48fbc7872e637ab1371bb73fe9a0
Pavlos <msf7415012@teilar.gr>
2017-10-26 05:21:51 +0300

    feat: add columns to pokemons contract

--- app/Library/Database/ContractDB.php
+++ app/Library/Database/ContractDB.php
@@ -5,12 +5,17 @@
  * Interfaces with tables names that contains constants
  * values for table name and columns names
  */
+
 namespace App\Library\Database;
 
-    interface PokemonsContract{
-        const TABLE_NAME = "pokemons";
-        const COLUMN_ID = "id";
-        const COLUMN_NAME = "name";
-        const COLUMN_URL = "url";
-    }
+interface PokemonsContract
+{
+    const TABLE_NAME = "pokemons";
+
+    const COLUMN_ID = "id";
+    const COLUMN_NAME = "name";
+    const COLUMN_URL = "url";
+    const COLUMN_CREATED_AT = "created_at";
+    const COLUMN_UPDATED_AT = "updated_at";
+}
 
