e35f1456b6836c31def7642cb8748501c48da1f3
Pavlos <msf7415012@teilar.gr>
2017-10-28 21:47:40 +0300

    feat: return values to list view

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Library\Database\PokemonProfilesContract;
+use App\Models\Pokemon;
 use Illuminate\Http\Request;
 
 
 class PokemonController extends Controller
 {
-    //
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function pokemonList()
     {
-        $pokemons = array(1,2,3);
+        $selectColumns = array(
+            PokemonProfilesContract::COLUMN_NAME,
+            PokemonProfilesContract::COLUMN_WEIGHT,
+            PokemonProfilesContract::COLUMN_IMAGE,
+            PokemonProfilesContract::COLUMN_HEIGHT,
+            PokemonProfilesContract::COLUMN_BASE_EXPERIENCE
+        );
+        $pokemons = Pokemon::orderBy(PokemonProfilesContract::COLUMN_WEIGHT, 'desc')
+            ->get($selectColumns);
+
         return view('list', ["pokemons" => $pokemons]);
     }
 }
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -2,5 +2,8 @@
 <html>
     <body>
         <h1>Hello, The name</h1>
+        @foreach ($pokemons as $pokemon)
+            <p>This is pokemon {{ $pokemon->name }}</p>
+        @endforeach
     </body>
 </html>
