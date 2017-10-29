8b68de972c7f78a46dff703e80c2282c06b3e78e
Pavlos <msf7415012@teilar.gr>
2017-10-29 11:44:28 +0200

    feat: pokemon table list

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,9 +1,45 @@
-<!doctype html>
-<html>
-    <body>
-        <h1>Hello, The name</h1>
-        @foreach ($pokemons as $pokemon)
-            <p>This is pokemon {{ $pokemon->name }}</p>
-        @endforeach
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>PokeKing</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <!-- Content here -->
+    <div class="row">
+            <table id="example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Height</th>
+                        <th>Weight</th>
+                        <th>Base Exp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($pokemons as $pokemon)
+                    <tr>
+                        <td><img src="{{ $pokemon->image }}" alt="{{ $pokemon->name }}" class="img-fluid"></td>
+                        <td>{{ $pokemon->name }}</td>
+                        <td>{{ $pokemon->height }}</td>
+                        <td>{{ $pokemon->weight }}</td>
+                        <td>{{ $pokemon->base_experience }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+    </div>
+</div>
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
 </html>
